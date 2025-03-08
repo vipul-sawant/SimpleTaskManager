469789c367154559b6ddab8561a1bd2918023ec4
Vipul Sawant <vipssawant842@gmail.com>
2025-03-08 16:53:26 +0530

    for react router added repo name as basename

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
 				element: <ProtectedRoutes> <NavbarComponent /> <UpdateTask /> </ProtectedRoutes>
 			}
 		]
-	}]);
+	}], { basename: "/SimpleTaskManager" });
 
     
 	useEffect(()=>{
